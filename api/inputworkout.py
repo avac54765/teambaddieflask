65965e7566ebba71551df05cc53a04514a3f6b4d
lydia-fastpage <lydia_cho@icloud.com>
2023-02-14 18:48:26 -0800

    fixes to inputworkouts and mainpy

--- api/inputworkout.py
+++ api/inputworkout.py
@@ -2,16 +2,16 @@ from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
 
-from model.inputworkouts import inputworkout
+from model.Inputworkouts import Inputworkout
 
-inputwork_api = Blueprint('InputWork_api', __name__,
-                   url_prefix='/api/InputWork')
+Inputworkout_api = Blueprint('Inputworkout_api', __name__,
+                   url_prefix='/api/Inputworkout')
 
-api = Api(inputwork_api)
+api = Api(Inputworkout_api)
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
 
-class InputWorkAPI:        
+class InputworkoutAPI:        
     class _Create(Resource):
         def post(self):
             ''' Read data for json body '''
@@ -33,23 +33,23 @@ class InputWorkAPI:
             if reps is None or len(reps) < 0:
                 return {'message': f'Input number of repetitions (must be integer)'}, 210
 
-            io = inputworkout(exerciseType=exerciseType,
+            io = Inputworkout(exerciseType=exerciseType,
                             sets=sets,
                             reps=reps,
                         )
             
-            inputworkout = io.create()
+            Inputworkout = io.create()
 
             # success returns json of user
-            if inputworkout:
-                return jsonify(inputworkout.read())
+            if Inputworkout:
+                return jsonify(Inputworkout.read())
             # failure returns error
             return {'message': f'Processed {exerciseType}, a format error'}, 210
     
     class _Read(Resource):
         def get(self):
-            inputworkouts = inputworkout.query.all()    # read/extract all users from database
-            json_ready = [inputworkout.read() for inputworkout in inputworkouts]  # prepare output in json
+            Inputworkouts = InputWork.query.all()    # read/extract all users from database
+            json_ready = [Inputworkout.read() for Inputworkout in Inputworkouts]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
 
     # building RESTapi endpoint
