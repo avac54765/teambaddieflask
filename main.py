e6073c6db84dc40aa7a5a5d9a099ac9610d79c3c
avac54765 <avabrynncarlson@icloud.com>
2023-02-14 17:11:47 -0800

    changing back to 8023

--- main.py
+++ main.py
@@ -63,4 +63,4 @@ if __name__ == "__main__":
     # change name for testing
     from flask_cors import CORS
     cors = CORS(app)
-    app.run(debug=True, host="0.0.0.0", port="4238")
+    app.run(debug=True, host="0.0.0.0", port="8023")
