c4bc3b2a072e65547033c9ebcebb7098d74bf170
nikhilc3 <nikhilchakravarthula14@gmail.com>
2023-02-28 08:49:35 -0800

    afafa

--- projects/projects.py
+++ projects/projects.py
@@ -21,3 +21,7 @@ def walruses():
 @app_projects.route('/hawkers/')
 def hawkers():
     return render_template("hawkers.html")
+
+@app_projects.route('/idk')
+def MYlogin():
+    return render_template("home.html")
